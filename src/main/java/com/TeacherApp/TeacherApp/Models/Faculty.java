9168e3c8081068632b9817d4081455d8d9211957
Jeeshan-Shrestha <shresthajeesan@gmail.com>
2025-11-25 13:00:05 +0545

    Initial

---
+++ src/main/java/com/TeacherApp/TeacherApp/Models/Faculty.java
@@ -0,0 +1,11 @@
+package com.TeacherApp.TeacherApp.Models;
+
+public enum Faculty {
+    COMPUTER,
+    CIVIL,
+    MECHANICAL,
+    ELECTRICAL,
+    AGRICULTURAL,
+    ELECTRONICS,
+    ARCHITECTURE
+}
---
+++ src/main/java/com/TeacherApp/TeacherApp/Models/Student.java
@@ -0,0 +1,39 @@
+package com.TeacherApp.TeacherApp.Models;
+
+import java.util.Collection;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection="Student")
+public class Student implements UserDetails{
+    
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private int semester;
+
+    private String section;
+
+    private Faculty faculty;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+    }
+    
+}
