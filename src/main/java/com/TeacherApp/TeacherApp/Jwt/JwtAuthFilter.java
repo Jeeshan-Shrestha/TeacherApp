34e83f82aa8d09c7619b37b33fd4fda3ba67d256
Jeeshan-Shrestha <shresthajeesan@gmail.com>
2025-11-26 13:42:19 +0545

    Added the jwt pre config files

---
+++ src/main/java/com/TeacherApp/TeacherApp/Jwt/JwtAuthFilter.java
@@ -0,0 +1,60 @@
+package com.TeacherApp.TeacherApp.Jwt;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.TeacherApp.TeacherApp.Services.MyUserDetailsService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter{
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+    
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+            String token = null;
+            String username = null;
+
+            Cookie[] cookies = request.getCookies();
+            for (Cookie cookie : cookies){
+                if (cookie.getName().equals("token")){
+                    token = cookie.getValue();
+                }
+            }
+
+            if (token != null && SecurityContextHolder.getContext().getAuthentication() == null){
+
+                username = jwtUtils.extractEmail(token);
+                if (username!= null){
+                    UserDetails userDetails = myUserDetailsService.loadUserByUsername(username);
+                    if (jwtUtils.validateToken(username, userDetails, token)){
+                        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                    }
+                }
+
+            }
+            filterChain.doFilter(request, response);
+    }
+    
+}
--- src/main/java/com/TeacherApp/TeacherApp/Jwt/JwtUtils.java
+++ src/main/java/com/TeacherApp/TeacherApp/Jwt/JwtUtils.java
@@ -1,5 +1,6 @@
 package com.TeacherApp.TeacherApp.Jwt;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,7 +12,11 @@ import org.springframework.stereotype.Component;
 import com.TeacherApp.TeacherApp.Models.Student;
 import com.TeacherApp.TeacherApp.Models.Teacher;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SecureDigestAlgorithm;
 
 @Component
 public class JwtUtils {
@@ -31,6 +36,38 @@ public class JwtUtils {
             claims.put("email", t.getEmail());
             claims.put("name",t.getName());
         }
+
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 30))
+                .claims(claims)
+                .signWith(key,SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Claims extractAllClaims(String token){
+        return Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+    }
+
+    public String extractUsername(String token) {
+        return (String)extractAllClaims(token).get("name");
+    }
+
+    public String extractEmail(String token){
+        return (String)extractAllClaims(token).get("email");
+    }
+
+    public boolean validateToken(String username, UserDetails userDetails, String token){
+        return username.equals(userDetails.getUsername()) && !isExpired(token);
+    }
+
+    private boolean isExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
     }
 
 }
